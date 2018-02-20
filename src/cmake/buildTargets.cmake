54ad0b72b768b00d6ba84885fa118f2f878172cc
Cédric Lemaitre <c.lemaitre58@gmail.com>
2018-02-20 10:18:31 +0100

    integrate coveralls change https://david-grs.github.io/cpp-clang-travis-cmake-gtest-coveralls-appveyor/

--- src/cmake/buildTargets.cmake
+++ src/cmake/buildTargets.cmake
@@ -1,3 +1,10 @@
+SET(COVERAGE OFF CACHE BOOL "Coverage")
+
+if (COVERAGE)
+	target_compile_options(tests PRIVATE --coverage)
+	target_link_libraries(tests PRIVATE --coverage)
+endif()
+
 include_directories(${PROJECT_SOURCE_DIR})
 include_directories(${PROJECT_BINARY_DIR})
 
