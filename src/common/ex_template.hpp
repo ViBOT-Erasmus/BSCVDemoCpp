a8d738cbcfaa88f2f34d55d9e7f03b8096813898
Cédric Lemaitre <c.lemaitre58@gmail.com>
2018-04-03 22:10:43 +0200

    example template!

---
+++ src/common/ex_template.hpp
@@ -0,0 +1,31 @@
+//  Copyright 2018 Cedric Lemaitre
+#ifndef SRC_COMMON_EX_TEMPLATE_HPP_
+#define SRC_COMMON_EX_TEMPLATE_HPP_
+
+#include <iostream>
+/// Template to print type of variable
+//
+//  Parameter :
+//  value : value to display
+template<typename T>
+void print_data(T value) {
+    std::cout << "The value is :" << value << "\n";
+}
+
+/// Template to add 2 operandes
+//
+//  Parameters :
+//
+//      n1 first operande
+//      n2 second operande
+//
+//
+//  return the same type T of parameter
+template<class T>
+T Add_T(T n1, T n2) {
+    T result;
+    result = n1 + n2;
+    return result;
+}
+
+#endif  //  SRC_COMMON_EX_TEMPLATE_HPP_
