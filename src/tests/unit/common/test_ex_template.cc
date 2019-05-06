7696bdfbb052bd217a5eed86ee7eac4572bd0334
Cédric Lemaitre <c.lemaitre58@gmail.com>
2019-05-06 09:29:15 +0200

    addd some tests for template

--- src/tests/unit/common/test_ex_template.cc
+++ src/tests/unit/common/test_ex_template.cc
@@ -3,14 +3,22 @@
 
 #include <cmath>
 
-#include <common/ex_template.hpp>
-#include <common/complex.hpp>
+#include "../../../common/ex_template.hpp"
+#include "../../../common/complex.hpp"
 // Include the gtest library
 
 // Create our own test to check the different function of our class
 // This function will take two arguments:
 // * The test case name
 // * The test name
+
+TEST(Add_T, test_int) {
+    int a = 2;
+    int b = 3;
+    int res = Add_T(a, b);
+    ASSERT_FLOAT_EQ(res, a+b);
+
+}
 
 TEST(Add_T, test_double) {
     double a = 2.13;
@@ -29,4 +37,21 @@ TEST(Add_T, test_complex) {
     complex res = Add_T(a, b);
     ASSERT_FLOAT_EQ(res.get_real(), ar+br);
     ASSERT_FLOAT_EQ(res.get_imag(), ai+bi);
+}
+
+TEST(print_data_template, test_int) {
+  int a = 10;
+  print_data(a);
+}
+
+TEST(print_data_template, test_double) {
+  double a = 10.10;
+  print_data(a);
+}
+
+TEST(print_data_template, test_complex) {
+  float ar = 2.13;
+  float ai = 3.17;
+  complex a(ar, ai);
+  print_data(a);
 }
